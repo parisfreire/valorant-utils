227c397cdf365227767f30b9e17d205f4483795c
Paris Freire <parisfreiregallego@gmail.com>
2020-05-15 19:35:35 +0100

    Updates

---
+++ precache-manifest.3848ed90b7997690c2bce8fd221c6b8b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da7ee763912e97758a93add9c9c0f37",
+    "url": "/valorant-utils/index.html"
+  },
+  {
+    "revision": "424014479dceb05638db",
+    "url": "/valorant-utils/static/css/main.ea923be0.chunk.css"
+  },
+  {
+    "revision": "c8e10b9e00a030ac1ce9",
+    "url": "/valorant-utils/static/js/2.3a81eef3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/valorant-utils/static/js/2.3a81eef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "424014479dceb05638db",
+    "url": "/valorant-utils/static/js/main.ef72da01.chunk.js"
+  },
+  {
+    "revision": "e5f2d73a3a97f4466777",
+    "url": "/valorant-utils/static/js/runtime-main.325d3804.js"
+  },
+  {
+    "revision": "04e02c9baf53307c7d9fcffbb1710124",
+    "url": "/valorant-utils/static/media/valorant-logo.04e02c9b.png"
+  },
+  {
+    "revision": "6bd698a73f612704abbb413feefad6f1",
+    "url": "/valorant-utils/static/media/valorant-title-logo.6bd698a7.png"
+  }
+]);
